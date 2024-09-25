6f8845d7341947363161a3c9f2ebf0d6cc6ee83d
whosefriendA <2653344989@qq.com>
2024-09-25 19:52:09 +0800

    new

--- test.go
+++ test.go
@@ -1,7 +1,45 @@
 package main
 
-import ("fmt" "math")
+import (
+	"fmt"
+)
 
-func main() {
-	
-}
+// func main() {
+// 	fmt.Println("helloworld")
+// 	for i:=0; i<5; i++{
+		
+// 	}
+// }
+// func main() {
+// 	i:=10
+// for {
+// 	i++
+// }
+// fmt.Println(i);
+// }
+// func Sqrt(x float64) float64 {
+// 	z:=1.0
+// 	for z1:=float64(0);z1!=z;{
+// 		z1=z
+// 		z-=(z*z-x)/(2*z)
+// 		fmt.Println(z)
+// 		fmt.Println(z1)
+// 	}
+// 	return z
+// }
+
+// func main() {
+// 	fmt.Println(Sqrt(2))
+// }
+// func main() {
+// 	i, j := 42, 2701
+
+// 	p := &i         // 指向 i
+// 	fmt.Println(*p) // 通过指针读取 i 的值
+// 	*p = 21         // 通过指针设置 i 的值
+// 	fmt.Println(i)  // 查看 i 的值
+
+// 	p = &j         // 指向 j
+// 	*p = *p / 37   // 通过指针对 j 进行除法运算
+// 	fmt.Println(j) // 查看 j 的值
+// }
